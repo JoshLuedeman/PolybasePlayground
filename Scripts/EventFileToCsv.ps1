c82e40e96b4619e9fb86ca2469efbd261ca0c094
Josh Luedeman <josh@joshluedeman.com>
2017-02-01 14:34:36 -0500

    Update Script with MIT License and moving team name and year into file

--- Scripts/EventFileToCsv.ps1
+++ Scripts/EventFileToCsv.ps1
@@ -1,4 +1,27 @@
-﻿# cd /
+﻿## -0,0 +1,21 @@
+#The MIT License (MIT) 
+#
+#Copyright (c) 2015 Microsoft
+# 
+#Permission is hereby granted, free of charge, to any person obtaining a copy 
+#of this software and associated documentation files (the "Software"), to deal 
+#in the Software without restriction, including without limitation the rights 
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+#copies of the Software, and to permit persons to whom the Software is 
+#furnished to do so, subject to the following conditions: 
+# 
+#The above copyright notice and this permission notice shall be included in all 
+#copies or substantial portions of the Software. 
+# 
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+#SOFTWARE. #
+
+cd /
 $decades = Get-ChildItem -Directory 
 
 foreach($decade in $decades)
@@ -10,9 +33,16 @@ foreach($decade in $decades)
     {
        $filename = $file.BaseName.ToString()
        $year = $filename.Substring(0,4)
+       $home_team = $filename.Substring(4,3)
 
        $command = ".\BEVENT.EXE -y $year -f 0-96 $file > $filename.csv"
        Invoke-Expression $command 
+       $full_filename = $filename + ".CSV"
+       
+       $data = Get-Content -Path $full_filename
+       $new_filename = "HDP_" + $filename + ".CSV"
+       $data|foreach {$_ + "," + $year + "," + $home_team} | Out-File $new_filename -Encoding utf8
+       Remove-Item -Path $full_filename
     }
     cd ..
 }
